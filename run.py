2595b4a13ecc53199327361257d39801dc554e74
TeslaTruther <adamvdb4@gmail.com>
2023-12-20 23:18:05 -0800

    update

--- run.py
+++ run.py
@@ -1,16 +1,28 @@
 import streamlit as st
 import pandas as pd
-import scipy.stats as stats
-from datetime import datetime
+from datetime import date, datetime, timedelta
 
-st.set_page_config(page_title="Free Hockey Locks", page_icon=":lock:", layout="wide")  
+st.set_page_config(page_title="Hockey Locks", page_icon="🔒", layout="wide")
 
-# Header section
-st.markdown("### Hockey Locks :lock:")
-st.markdown("Last Data Update: 12/19/23")   
-st.write("Shoutout Cam for always riding")
-st.write("How the model works: Compare these odds to your sportsbook's odds. If my projected odds are lower than the sportsbook's odds place the bet.")  
-st.write("Things to consider: The projected odds do not include rake so will often be higher then your sportsbook showing no value. The projected odds do not consider back to back or injuries which could show false value.")
+# Custom CSS for styling the sidebar
+custom_sidebar_css = """
+    <style>
+        .sidebar .sidebar-content {
+            width: 200px;  /* Adjust the width as needed */
+            background-color: #333;  /* Sidebar background color */
+        }
+
+        .sidebar .sidebar-content .stRadio {
+            color: white;  /* Radio button text color */
+        }
+    </style>
+"""
+
+# Apply custom CSS to the sidebar
+st.markdown(custom_sidebar_css, unsafe_allow_html=True)
+
+# Set the time zone offset to 8 hours back (UTC-8)
+selected_time_zone_offset = -8
 
 # Use a relative path to the Excel file
 excel_file = '2024 Schedule.xlsx'
@@ -22,15 +34,14 @@ wins_data = pd.read_excel(excel_file, sheet_name="Test Data")
 # Get today's date dynamically
 today = datetime.now().date()
 
-# Convert 'today' to a Pandas Timestamp object
-today_timestamp = pd.Timestamp(today)
+# Convert 'today' to a Pandas Timestamp object and adjust for the selected time zone offset
+today_timestamp = pd.Timestamp(today) + timedelta(hours=selected_time_zone_offset)
 
 # Filter the DataFrame to get today's games
 today_games = df[(df['Date'] >= today_timestamp) & (df['Date'] < today_timestamp + pd.DateOffset(1))]
 
 tomorrow_timestamp = today_timestamp + pd.DateOffset(1)
 tomorrow_games = df[(df['Date'] >= tomorrow_timestamp) & (df['Date'] < tomorrow_timestamp + pd.DateOffset(1))]
-
 
 def determine_winner(home_team, visitor_team, wins_data, home_goal_advantage=0.18):
     home_srs = pd.to_numeric(wins_data[wins_data['Team'] == home_team]['Average SRS'], errors='coerce')
@@ -94,73 +105,103 @@ def implied_probability_to_decimal_odds(probability):
 game_odds = []
 
 
-# Button to get today's odds
-if st.button("Get Today's Odds", key="get_today_odds"):
-    for index, row in today_games.iterrows():
-        home_team = row['Home']
-        visitor_team = row['Visitor']
 
-        winner, delta = determine_winner(home_team, visitor_team, wins_data, home_goal_advantage=0.18)
 
-        # Calculate the implied probabilities and odds
-        margin_of_victory = delta
-        win_prob, lose_prob, tie_prob = implied_probabilities(margin_of_victory)
+# Apply custom CSS to the sidebar
+st.markdown(custom_sidebar_css, unsafe_allow_html=True)
 
-        # Convert adjusted probabilities to decimal odds
-        decimal_win_odds = implied_probability_to_decimal_odds(win_prob)
-        decimal_lose_odds = implied_probability_to_decimal_odds(lose_prob)
+# Sidebar with a smaller width
+selection = st.sidebar.radio('Hockey Locks :lock:', ['Home', 'Hockey Model', 'Performance Tracking'])
 
-        game_odds.append({
-            'Home Team': home_team,
-            'Visitor Team': visitor_team,
-            'Winner': winner,
-            'Delta': delta,
-            'Implied Win Probability': win_prob,
-            'Implied Lose Probability': lose_prob,
-            'Decimal Win Odds': decimal_win_odds,
-            'Decimal Lose Odds': decimal_lose_odds
-        })
+if selection == 'Home':
+    # Main content
+    st.title("Welcome to Hockey Locks :lock:")
+    st.write("This hockey model calculates expected hockey odds by regressing a series of analytics over the last 2 seasons.")
+    st.write("Use this tool to catch your bookie sleeping and make positive EV hockey bets.")           
 
-    # Display the odds for today's games in a Streamlit table
-    st.write("## Odds for Today's Games:")
-    for i, game in enumerate(game_odds, start=1):
-        st.write(f"### Game {i}:")
-        st.write(f"**Home Team:** {game['Home Team']} | **Projected Odds:** {game['Decimal Win Odds']:.3f}")
-        st.write(f"**Visitor Team:** {game['Visitor Team']} | **Projected Odds:** {game['Decimal Lose Odds']:.3f}")
-        st.write("\n")
+elif selection == 'Hockey Model':
 
-# Button to get tomorrow's odds
-if st.button("Get Tomorrow's Odds", key="get_tomorrow_odds"):
-    game_odds = []  # Reset the list for tomorrow's odds
-    for index, row in tomorrow_games.iterrows():
-        home_team = row['Home']
-        visitor_team = row['Visitor']
+    st.header("How the Model Works")
+    st.write("Compare these odds to your sportsbook's odds. If my projected odds are lower than the sportsbook's odds, place the bet.")
+    st.subheader("Model Considerations")
+    st.write("The model does not include rake by design.")
+    st.write("The model does not incorporate back-to-back games or injuries. These statistically significant varibles need to be adjusted manually.")
+    st.subheader("Run The Model:")
+    # Button to get today's odds
+    if st.button("Generate Today's Odds", key="get_today_odds"):
+        for index, row in today_games.iterrows():
+            home_team = row['Home']
+            visitor_team = row['Visitor']
 
-        winner, delta = determine_winner(home_team, visitor_team, wins_data, home_goal_advantage=0.18)
+            winner, delta = determine_winner(home_team, visitor_team, wins_data, home_goal_advantage=0.18)
 
-        # Calculate the implied probabilities and odds
-        margin_of_victory = delta
-        win_prob, lose_prob, tie_prob = implied_probabilities(margin_of_victory)
+            # Calculate the implied probabilities and odds
+            margin_of_victory = delta
+            win_prob, lose_prob, tie_prob = implied_probabilities(margin_of_victory)
 
-        # Convert adjusted probabilities to decimal odds
-        decimal_win_odds = implied_probability_to_decimal_odds(win_prob)
-        decimal_lose_odds = implied_probability_to_decimal_odds(lose_prob)
+            # Convert adjusted probabilities to decimal odds
+            decimal_win_odds = implied_probability_to_decimal_odds(win_prob)
+            decimal_lose_odds = implied_probability_to_decimal_odds(lose_prob)
 
-        game_odds.append({
-            'Home Team': home_team,
-            'Visitor Team': visitor_team,
-            'Winner': winner,
-            'Delta': delta,
-            'Implied Win Probability': win_prob,
-            'Implied Lose Probability': lose_prob,
-            'Decimal Win Odds': decimal_win_odds,
-            'Decimal Lose Odds': decimal_lose_odds
-        })
+            game_odds.append({
+                'Home Team': home_team,
+                'Visitor Team': visitor_team,
+                'Winner': winner,
+                'Delta': delta,
+                'Implied Win Probability': win_prob,
+                'Implied Lose Probability': lose_prob,
+                'Decimal Win Odds': decimal_win_odds,
+                'Decimal Lose Odds': decimal_lose_odds
+            })
 
-    # Display the odds for tomorrow's games in a Streamlit table
-    st.write("## Odds for Tomorrow's Games:")
-    for i, game in enumerate(game_odds, start=1):
-        st.write(f"### Game {i}:")
-        st.write(f"**Home Team:** {game['Home Team']} | **Projected Odds:** {game['Decimal Win Odds']:.3f}")
-        st.write(f"**Visitor Team:** {game['Visitor Team']} | **Projected Odds:** {game['Decimal Lose Odds']:.3f}")
-        st.write("\n")
+        # Display the odds for today's games in a Streamlit table
+        
+        for i, game in enumerate(game_odds, start=1):
+            st.write(f"### Game {i}:")
+            st.write(f"**Home Team:** {game['Home Team']} | **Projected Odds:** {game['Decimal Win Odds']:.3f}")
+            st.write(f"**Visitor Team:** {game['Visitor Team']} | **Projected Odds:** {game['Decimal Lose Odds']:.3f}")
+            st.write("\n")
+
+    # Button to get tomorrow's odds
+    if st.button("Generate Tomorrow's Odds", key="get_tomorrow_odds"):
+        game_odds = []  # Reset the list for tomorrow's odds
+        for index, row in tomorrow_games.iterrows():
+            home_team = row['Home']
+            visitor_team = row['Visitor']
+
+            winner, delta = determine_winner(home_team, visitor_team, wins_data, home_goal_advantage=0.18)
+
+            # Calculate the implied probabilities and odds
+            margin_of_victory = delta
+            win_prob, lose_prob, tie_prob = implied_probabilities(margin_of_victory)
+
+            # Convert adjusted probabilities to decimal odds
+            decimal_win_odds = implied_probability_to_decimal_odds(win_prob)
+            decimal_lose_odds = implied_probability_to_decimal_odds(lose_prob)
+
+            game_odds.append({
+                'Home Team': home_team,
+                'Visitor Team': visitor_team,
+                'Winner': winner,
+                'Delta': delta,
+                'Implied Win Probability': win_prob,
+                'Implied Lose Probability': lose_prob,
+                'Decimal Win Odds': decimal_win_odds,
+                'Decimal Lose Odds': decimal_lose_odds
+            })
+
+        # Display the odds for tomorrow's games in a Streamlit table
+        
+        for i, game in enumerate(game_odds, start=1):
+            st.write(f"### Game {i}:")
+            st.write(f"**Home Team:** {game['Home Team']} | **Projected Odds:** {game['Decimal Win Odds']:.3f}")
+            st.write(f"**Visitor Team:** {game['Visitor Team']} | **Projected Odds:** {game['Decimal Lose Odds']:.3f}")
+            st.write("\n")
+
+        
+
+elif selection == 'Performance Tracking':
+    st.write('ACCUMULATING GAINS')
+
+
+
